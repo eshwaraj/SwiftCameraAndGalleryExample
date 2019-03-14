1ec5ee1ba0c27f8534d4ace9b1f1bcd241b1cfc4
MacStudent <macstudent@126-iMac31.local>
2019-03-13 20:59:54 -0400

    final

--- SwiftCameraAndGalleryExample/ViewController.swift
+++ SwiftCameraAndGalleryExample/ViewController.swift
@@ -7,12 +7,14 @@
 //
 
 import UIKit
-
-class ViewController: UIViewController, UIImagePickerControllerDelegate,UINavigationControllerDelegate
+import MobileCoreServices
+class ViewController: UIViewController
 {
 
     @IBOutlet weak var lblSelect: UILabel!
     @IBOutlet weak var ImagePic: UIImageView!
+    var newMedia: Bool?
+    
     override func viewDidLoad() {
       
         super.viewDidLoad()
@@ -24,7 +26,10 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate,UINaviga
             let myPickerController = UIImagePickerController()
             myPickerController.delegate = self;
             myPickerController.sourceType = .camera
+            myPickerController.mediaTypes = [kUTTypeImage as String]
+            myPickerController.allowsEditing = false
             self.present(myPickerController, animated: true, completion: nil)
+            newMedia = true
         }
     }
     @IBAction func GalleryButton(_ sender: UIBarButtonItem) {
@@ -34,20 +39,53 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate,UINaviga
             let myPickerController = UIImagePickerController()
             myPickerController.delegate = self;
             myPickerController.sourceType = .photoLibrary
+            myPickerController.mediaTypes = [kUTTypeImage as String]
+            myPickerController.allowsEditing = false
             self.present(myPickerController, animated: true, completion: nil)
+            newMedia = false
         }
     }
+}
+extension ViewController:  UIImagePickerControllerDelegate, UINavigationControllerDelegate
+{
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
         self.dismiss(animated: true, completion: nil)
     }
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let mediaType = info[UIImagePickerController.InfoKey.mediaType] as! NSString
         if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
             self.ImagePic.image = image
+            if (newMedia == true) {
+                UIImageWriteToSavedPhotosAlbum(image, self, #selector(self.image(image:didFinishSavingWithError:contextInfo:)), nil)
+            } else if mediaType.isEqual(to: kUTTypeMovie as String) {
+                
+            }
         }else{
             print("Something went wrong")
         }
+        for (k, _) in info {
+            print("TEST - \(k)")
+        }
         self.dismiss(animated: true, completion: nil)
     }
-    }
+    
+
+    
+    @objc func image(image: UIImage, didFinishSavingWithError error: NSErrorPointer, contextInfo:UnsafeRawPointer) {
+        
+        if error != nil {
+            let alert = UIAlertController(title: "Save Failed",
+                                          message: "Failed to save image",
+                                          preferredStyle: UIAlertController.Style.alert)
+            
+            let cancelAction = UIAlertAction(title: "OK",
+                                             style: .cancel, handler: nil)
+            
+            alert.addAction(cancelAction)
+            self.present(alert, animated: true,
+                         completion: nil)
+}
+}
+}
 
 
